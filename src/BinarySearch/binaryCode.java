7a5bf621a2ea9b0c34e276cb4f540ac9eb4b1449
Sahil Agrawal <agrawalsahil4994@gmail.com>
2023-10-07 21:04:10 +0530

    Source Contributions

---
+++ src/BinarySearch/binaryCode.java
@@ -0,0 +1,21 @@
+package BinarySearch;
+
+public class binaryCode {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,5,6,7,9};
+        int target = 4;
+        System.out.println(search(arr,target));
+    }
+    static int search(int[] arr,int target){
+        int start =0;
+        int end = arr.length-1;
+
+        while(start<=end){
+            int mid = (start + end)/2;
+            if(arr[mid]==target) return mid;
+            else if(arr[mid]< target) start = mid+1;
+            else end = mid-1;
+
+        } return end;
+    }
+}
